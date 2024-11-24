f91b67baa8fad92bd635e93dbee6a0d7eafa4651
Mohan <moveraitch@gmail.com>
2024-11-24 00:39:38 +0000

    post css

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -8,9 +8,12 @@
 // export default config;
 
 
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+export default {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
 }
